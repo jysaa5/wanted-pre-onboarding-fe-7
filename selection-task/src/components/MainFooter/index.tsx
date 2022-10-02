8e060dfa9177cba8268b518226c75214e1ec3a60
jysaa5 <jysaa5@naver.com>
2022-10-02 21:18:19 +0900

    Revert "Revert "update project""
    This reverts commit 3ed2f1f36258a58be5a5f74c9ca83af04a6d64f3.

---
+++ selection-task/src/components/MainFooter/index.tsx
@@ -0,0 +1,8 @@
+import { Layout } from 'antd';
+const { Footer } = Layout;
+
+const MainFooter = () => {
+  return <Footer style={{ textAlign: 'center' }}>원티드 프론트엔드 선발 과제 2022</Footer>;
+};
+
+export default MainFooter;
